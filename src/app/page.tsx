32d4a26bc0ace6e6a4ac08fd041aed8c4c3902a2
Hiro-824 <ma8950246@gmail.com>
2025-04-05 15:50:35 +0900

    Vercel Deployment Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import SentenceBuilder from "@/component/sentence-builder";
 
 export default function Home() {
   return (
-    <>
-        <SentenceBuilder />
-    </>
+    <SentenceBuilder />
   );
 }
