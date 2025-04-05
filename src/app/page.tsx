6cd94cc2e1e028c4895e06f3db01acce5a9985d0
Hiro-824 <ma8950246@gmail.com>
2025-04-05 15:53:32 +0900

    Update check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import SentenceBuilder from "@/component/sentence-builder";
 export default function Home() {
   return (
     <>
+      <text>This page has been updated!</text>
       <SentenceBuilder />
     </>
   );
