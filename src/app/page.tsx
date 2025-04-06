ad87939fdafb5c115775160d0d8e1353f6f5a9a2
Hiro-824 <thefilepreserver@gmail.com>
2025-04-06 16:43:34 +0900

    Remove outdated text from the home page component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import SentenceBuilder from "@/component/sentence-builder";
 export default function Home() {
   return (
     <>
-      <text>This page has been updated (Second)!</text>
       <SentenceBuilder />
     </>
   );
