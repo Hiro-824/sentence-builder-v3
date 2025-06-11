422f644d5b97693e5a16b9a86a5527d25f82c145
Hiro-824 <thefilepreserver@gmail.com>
2025-06-11 22:04:15 +0900

    Separate grammar related interface to grammar-entities.ts

---
+++ src/models/grammar-entities.ts
@@ -0,0 +1,18 @@
+export type FeatureValue = string | number | boolean | FeatureStructure;
+export interface FeatureStructure { [key: string]: FeatureValue; }
+
+export interface Phrase {
+    head: FeatureStructure;
+    left?: Phrase[];
+    right?: Phrase[];
+    gaps?: Phrase[];
+    categoryName?: string;
+    leftModTargets?: Phrase[];
+    rightModTargets?: Phrase[];
+}
+
+export interface Word { token: string; categories: Phrase[]; }
+export const MissingArgument: Word = { token: "[[MISSING_ARGUMENT]]", categories: [] };
+
+export type RecursiveParseElement = Word | SubPhraseInput;
+export interface SubPhraseInput { elements: RecursiveParseElement[]; headIndex: number; phraseName?: string; }
