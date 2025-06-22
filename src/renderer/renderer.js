eb6bf8ef4a0cd83164a19262751b2b0119a0ba14
Hiro-824 <thefilepreserver@gmail.com>
2025-06-22 14:37:31 +0900

    Update viewport height calculation to use window height directly for improved layout consistency.

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -10,7 +10,7 @@ export class Renderer {
         this.blockList = blockList;
         this.svg = svg;
         this.sideBarScrollExtent = 0;
-        this.viewportHeight = Math.min(window.innerHeight, window.innerWidth);
+        this.viewportHeight = window.innerHeight
         this.converter = new Converter;
         this.grammar = new Grammar;
         this.render();
