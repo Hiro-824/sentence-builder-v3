dc0d718c71b04cad105f4e69ad6625a6c78cd20b
Hiro-824 <thefilepreserver@gmail.com>
2025-07-12 20:40:11 +0900

    Move determiners to a separate file.

---
+++ src/data/determiners.ts
@@ -0,0 +1,69 @@
+import { Block } from "@/models/block"
+import { det, noun } from "@/models/grammar-entities"
+
+export const blockThe: Block = {
+    id: "",
+    x: 0,
+    y: 0,
+    words: [{
+        token: "",
+        categories: [{
+            head: { type: det, agr: {}, determinered: true },
+            right: [{
+                head: { type: noun, agr: {} }
+            }],
+            customUnification: [
+                [["head", "agr"], ["right", 0, "head", "agr"]]
+            ],
+            translationTemplates: {
+                default: ["(その)", { path: ["right", 0], key: "default" }]
+            }
+        }]
+    }],
+    color: "dodgerblue",
+    isRound: true,
+    children: [{
+        id: "head",
+        hidden: false,
+        type: "text",
+        content: ["the"],
+    },
+    {
+        id: "complement",
+        hidden: false,
+        type: "placeholder",
+        content: null,
+    }]
+}
+
+export const blockA: Block = {
+    id: "",
+    x: 0,
+    y: 0,
+    words: [{
+        token: "",
+        categories: [{
+            head: { type: det, agr: { type: "3sing" }, determinered: true },
+            right: [{
+                head: { type: noun, agr: { type: "3sing" }, count: true }
+            }],
+            translationTemplates: {
+                default: ["(ある)", { path: ["right", 0], key: "default" }]
+            }
+        }]
+    }],
+    color: "dodgerblue",
+    isRound: true,
+    children: [{
+        id: "head",
+        hidden: false,
+        type: "text",
+        content: ["a/an"],
+    },
+    {
+        id: "complement",
+        hidden: false,
+        type: "placeholder",
+        content: null,
+    }]
+}
