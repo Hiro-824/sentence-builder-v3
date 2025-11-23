968dab7f5ae27be31e49559a3f1a573d9cb4dc03
Hiro-824 <thefilepreserver@gmail.com>
2025-11-23 20:42:15 +0900

    refactor: Clean up imports in blocks.ts for improved organization
    - Removed unused block imports from pronouns, nouns, and auxiliaries to streamline the code.
    - Enhanced readability and maintainability of the blocks file by consolidating import statements.

--- src/data/blocks.ts
+++ src/data/blocks.ts
@@ -1,10 +1,10 @@
 import { Block } from "@/models/block";
-import { allPronounBlocks, blockI } from "./pronouns";
-import { allNounBlocks, blockMary, blockName } from "./nouns";
+import { allPronounBlocks } from "./pronouns";
+import { allNounBlocks } from "./nouns";
 import { allVerbBlocks } from "./verbs";
 import { allAdjectiveBlocks } from "./adjectives";
 import { blockA, blockMany, blockS, blockThe } from "./determiners";
-import { allAuxiliaryBlocks, blockBe } from "./auxiliaries";
+import { allAuxiliaryBlocks } from "./auxiliaries";
 import { allPrepositionBlocks } from "./prepositions";
 import { blockBecause, blockHowManySentence, blockHowSentence, blockThat, blockWhatDetSentence, blockWhatSentence, blockWhereSentence, blockWhichDetSentence, blockWhichSentence, blockWhoseDetSentence, blockWhoSentence, blockWhoseSentence } from "./complementizers";
 import { blockActively, blockAfterSchool, blockAggressively, blockAlready, blockAlways, blockAround, blockBadly, blockCarefully, blockCalmly, blockClearly, blockEasily, blockEfficiently, blockEnergetically, blockEvery, blockEverywhere, blockFar, blockFast, blockFinally, blockHardAdv, blockHere, blockHappily, blockIndoors, blockJust, blockLateAdv, blockLater, blockLoudly, blockMaybe, blockNear, blockNever, blockNow, blockOften, blockOnline, blockOutdoors, blockPolitelyAdv, blockProbably, blockProfessionally, blockQuickly, blockQuietly, blockRarely, blockReally, blockRegularly, blockSafely, blockSeriouslyAdv, blockSlowly, blockSmoothly, blockSometimes, blockSoon, blockStill, blockThen, blockThere, blockTogether, blockTomorrow, blockToday, blockUsually, blockVery, blockWell, blockYesterday, blockEarlyAdv } from "./adverbials";
